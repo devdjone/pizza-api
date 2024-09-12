657cd4248aa73694d08a5506db13014b1a9722b7
djdev <iamdevcode@gmail.com>
2024-09-11 23:15:54 -0600

    remove 8080

--- pizza-api/Program.cs
+++ pizza-api/Program.cs
@@ -7,7 +7,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.WebHost.UseUrls("http://*:8080");
+//builder.WebHost.UseUrls("http://*:8080");
 
 var app = builder.Build();
 
