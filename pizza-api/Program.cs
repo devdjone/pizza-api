03f17a8bd4cc563b85b8d683375d52edb8f3fc7e
djdev <iamdevcode@gmail.com>
2024-09-15 18:21:25 -0600

    add 8080

--- pizza-api/Program.cs
+++ pizza-api/Program.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//builder.Configuration.AddUserSecrets(Assembly.GetExecutingAssembly(), true);
+//builder.Configuration.AddEnvironmentVariables();
 
-//builder.WebHost.UseUrls("http://*:8080");
+
+builder.WebHost.UseUrls("http://*:8080");
 
 var app = builder.Build();
 
@@ -16,7 +21,12 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    //builder.Configuration.AddUserSecrets(Assembly.GetExecutingAssembly(), optional: true);
 }
+
+builder.Configuration.AddEnvironmentVariables();
+
+//https://dontpaniclabs.com/blog/post/2023/03/02/how-to-set-up-user-secrets-for-net-core-projects-in-visual-studio/#:~:text=To%20generate%20your%20user%20secrets,use%20should%20be%20stored%20here.
 
 app.UseHttpsRedirection();
 
