01f2335769500a6f6f7c491bae92e92bfe15f3cc
djdev <iamdevcode@gmail.com>
2024-09-15 19:26:33 -0600

    add hello

--- pizza-api/Controllers/HelloController.cs
+++ pizza-api/Controllers/HelloController.cs
@@ -4,9 +4,9 @@ namespace pizza_api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class HomeController : ControllerBase
+    public class HelloController : ControllerBase
     {
-        [HttpGet(Name = "GetHome")]
+        [HttpGet(Name = "GetIndex")]
         public string Index()
         {
             return "hellow 4";
