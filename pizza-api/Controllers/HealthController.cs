92e3d742f72c8f4713f772ef6c16f0dbee6ed8ff
djdev <iamdevcode@gmail.com>
2024-10-12 18:42:38 -0600

    add health endpoint

---
+++ pizza-api/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace pizza_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet()]
+        public string Index()
+        {
+            return "Up";
+        }
+    }
+}
