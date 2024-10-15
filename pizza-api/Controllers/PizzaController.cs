066aa3bf648e3d372f61481c6fa39733c61067a9
djdev <iamdevcode@gmail.com>
2024-10-14 20:22:02 -0600

    pizza 3

--- pizza-api/Controllers/PizzaController.cs
+++ pizza-api/Controllers/PizzaController.cs
@@ -21,25 +21,31 @@ namespace pizza_api.Controllers
             _context = context;
         }
 
-        [HttpGet(Name = "Index2")]
+        //[HttpGet(Name = "Index2")]
+        //public async Task<List<Pizza>> Index()
+        //{
+        //    await Task.Delay(0);
+        //    //return await _context.Pizza.ToListAsync();
+        //    var p = new Pizza();
+        //    p.Id = 1;
+        //    p.Name = "peperon";
+        //    p.Qty = 5;
 
-        public async Task<List<Pizza>> Index()
+        //    var list = new List<Pizza>();
+        //    list.Add(p);
+        //    return list;
+        //}
+
+        [HttpGet(Name = "Index3")]
+        public async Task<List<Pizza>> Index2()
         {
             await Task.Delay(0);
-            //return await _context.Pizza.ToListAsync();
-            var p = new Pizza();
-            p.Id = 1;
-            p.Name = "peperon";
-            p.Qty = 5;
-
-            var list = new List<Pizza>();
-            list.Add(p);
-            return list;
+            return await _context.Pizza.ToListAsync();
+            
         }
 
-
         //[HttpGet(Name = "pizza/my")]
-        
+
         //public string order()
         //{
         //    return "order pizza";
